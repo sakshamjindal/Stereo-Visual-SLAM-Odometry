b5181ef33f84f27248db43497485c84dfe9e0a37
= <saksham.jindal@outlook.com>
2021-01-16 00:59:19 +0000

    Reformatted and added comments in main code

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np 
-import cv2
 import argparse
 import matplotlib.pyplot as plt
 
@@ -8,11 +6,13 @@ from stereoVO.datasets import KittiDataset
 from stereoVO.utils import SVO_Plot, rmse_error
 from stereoVO.model import StereoVO
 
+
 def parse_argument():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_path', default='configs/params.yaml')
     return parser.parse_args()
+
 
 def main():
 
@@ -36,18 +36,25 @@ def main():
 
     # Iterate over the frame and update the rotation and translation vector
     for index in range(num_frames):
-        
+
         left_frame, right_frame, ground_pose = dataset[index]
+
+        # Do model prediction
         pred_location, pred_orientation = model(left_frame, right_frame, index)
+
+        # Calculate error
         rmse = rmse_error(pred_location, ground_pose[:, -1])
 
+        # Plot camera trajectory, frame and error
         Plotter.plot_camera_traectories(index, pred_location, pred_orientation, ground_pose)
         Plotter.plot_errors(index, rmse)
         Plotter.plot_frame(left_frame)
 
+        # Clear plot and prepare for next iterations
         Plotter.clear()
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
